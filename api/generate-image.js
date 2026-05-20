9ec6b965188577f360708468a7de17c04ea1818f
robertnotario06 <125972736+robertnotario06@users.noreply.github.com>
2026-05-20 22:36:01 +0200

    Update generate-image.js

--- api/generate-image.js
+++ api/generate-image.js
@@ -25,10 +25,10 @@ module.exports = async function handler(req, res) {
         'Authorization': `Bearer ${apiKey}`
       },
       body: JSON.stringify({
-        model: 'dall-e-3',
+        model: 'dall-e-2',
         prompt: prompt,
         n: 1,
-        size: '1024x1024',
+        size: '512x512',
         quality: 'standard'
       })
     });
